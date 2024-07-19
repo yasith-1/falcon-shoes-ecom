2a8301684f903cf446dae60a82ae8412a27cd6eb
Yasith <141606159+yasith-1@users.noreply.github.com>
2024-07-19 10:04:33 +0530

    #

--- script.js
+++ script.js
@@ -1219,30 +1219,7 @@ function advSearchProduct(x) {
 
 
 
-// // Signle product view
 
-// const imgs = document.querySelectorAll('.img-select a');
-// const imgBtns = [...imgs];
-// let imgId = 1;
-
-// imgBtns.forEach((imgItem) => {
-//     imgItem.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         imgId = imgItem.dataset.id;
-//         slideImage();
-//     });
-// });
-
-// function slideImage() {
-//     const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
-
-//     document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
-// }
-
-// window.addEventListener('resize', slideImage);
-
-
-// // Signle product view
 
 
 
